cda18f109d77c057d3ea6fee4ad9a8ff08e59c7a
chawthinzar <chawthinzar@cybermissions.co.jp>
2021-12-15 10:52:33 +0900

    commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,26 @@
+import Layout from "../components/layout"
+import * as style from "../styles/contact.module.scss" 
+
 const Contact = () => {
     return (
-        <h1>Contact Page</h1>
+        <Layout>
+            
+            <div className={style.wrapper}>
+                <div className={style.container}>
+                    <h1>Contact</h1>
+                    <p>お気軽にご連絡ください</p>
+                    <form action="https://formspree.io/f/mlefwfbp" method="POST">
+                        <label htmlFor="name">お名前</label>  
+                        <input type="text" name="name" id="name" required/>
+                        <label htmlFor="email">メールアドレス</label>
+                        <input type="email" name="email" id="email" required/>
+                        <label htmlFor="textarea">ご用件</label>
+                        <textarea name="message" rows="10" id="textarea" required></textarea>
+                        <button type="submit">送信</button>
+                    </form> 
+                </div>
+            </div>
+        </Layout>
     )
 }
 
